5a88c30148d29b5d158fa55bb9a064a17d1699fa
doublik <doublik99@gmail.com>
2019-07-10 19:43:28 +0900

    Solve BOJ: 10953(A+B - 6)

---
+++ NOJAM/10953.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+using namespace std;
+typedef long long ll;
+const ll INF = 2e9;
+const ll MOD = 1e9+7;
+
+int main() {
+  int t;
+  scanf("%d",&t);
+  while(t--) {
+      int a,b;
+      scanf("%d,%d",&a,&b);
+      printf("%d\n",a+b);
+  }
+  return 0;
+}
