3d5aca25a3d9ca67ff02b6ab2d5b25727e821700
doublik <doublik99@gmail.com>
2019-07-10 19:48:29 +0900

    Solve BOJ: 11021(A+B - 7)

---
+++ NOJAM/11021.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+using namespace std;
+typedef long long ll;
+const ll INF = 2e9;
+const ll MOD = 1e9+7;
+
+int main() {
+  int t;
+  scanf("%d",&t);
+  for(int i=1; i<=t; i++) {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("Case #%d: %d\n",i,a+b);
+  }
+  return 0;
+}
