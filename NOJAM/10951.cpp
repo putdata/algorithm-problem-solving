1d7e16cb32777e7a67c1999d718118335977d0ec
doublik <doublik99@gmail.com>
2019-07-10 19:39:39 +0900

    Solve BOJ: 10951(A+B - 4)

---
+++ NOJAM/10951.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+using namespace std;
+typedef long long ll;
+const ll INF = 2e9;
+const ll MOD = 1e9+7;
+
+int main() {
+  int A,B;
+  while(scanf("%d%d",&A,&B)==2) printf("%d\n",A+B);
+  return 0;
+}
