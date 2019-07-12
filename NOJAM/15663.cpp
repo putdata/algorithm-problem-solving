c054d663576c0269ac2ddcbff6bd396d994c2b94
doublik <doublik99@gmail.com>
2019-07-13 02:14:01 +0900

    Solve BOJ: 15663(N과 M (9))

---
+++ NOJAM/15663.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,M;
+int arr[10];
+bool visit[10];
+set<string> sat;
+void foo(int y, string str) {
+  if (y==M) {
+    cout << str << '\n';
+  } else {
+    int b=0;
+    for (int i=0; i<N; i++) {
+      if(visit[i] || (i && b==arr[i])) continue;
+      b=arr[i];
+      visit[i]=1;
+      foo(y+1,str+to_string(arr[i])+' ');
+      visit[i]=0;
+    }
+  }
+}
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  cin >> N >> M;
+  for(int i=0; i<N; i++) cin >> arr[i];
+  sort(arr, arr+N);
+  foo(0,"");
+  return 0;
+}
