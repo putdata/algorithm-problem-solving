4f447d3a1fc014be56ffd6ce96d3397e0f9ea671
doublik <doublik99@gmail.com>
2019-07-13 01:37:11 +0900

    Solve BOJ: 15657(N과 M (8))

---
+++ NOJAM/15657.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,M;
+int arr[10];
+void foo(int x, int y, string str) {
+  if (y==M) {
+    cout << str << '\n';
+  } else {
+    for (int i=x; i<N; i++) {
+      foo(i,y+1,str+to_string(arr[i])+' ');
+    }
+  }
+}
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  cin >> N >> M;
+  for(int i=0; i<N; i++) cin >> arr[i];
+  sort(arr, arr+N);
+  foo(0,0,"");
+  return 0;
+}
