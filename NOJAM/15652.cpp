0dcb7e7fef67cecef974b6671c5aed9833e3da7d
doublik <doublik99@gmail.com>
2019-07-13 01:05:13 +0900

    Solve BOJ: 15652(N과 M (4))

---
+++ NOJAM/15652.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,M;
+char ans[20];
+void foo(int x, int y) {
+  if (y==M) {
+    cout << ans << '\n';
+  } else {
+    for (int i=x; i<=N; i++) {
+      ans[y*2]=i+'0';
+      foo(i,y+1);
+    }
+  }
+}
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  cin >> N >> M;
+  for(int i=0; i<M*2; i++) ans[i]=' ';
+  foo(1,0);
+  return 0;
+}
