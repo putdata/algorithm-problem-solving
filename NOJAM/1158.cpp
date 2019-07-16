07e60d12083e521a6aa4283b20503a23d9dbd56d
doublik <doublik99@gmail.com>
2019-07-17 00:40:38 +0900

    Solve BOJ: 1158(조세퍼스 문제)

---
+++ NOJAM/1158.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,K,cnt;
+queue<int> que;
+int main() {
+  cin >> N >> K;
+  for(int i=1; i<=N; i++) que.push(i);
+  cout << '<';
+  while(que.size()!=1) {
+    int t=que.front();
+    que.pop();
+    if(++cnt==K) {
+      cout << t << ", ";
+      cnt=0;
+    } else que.push(t);
+  }
+  cout << que.front() << '>';
+  return 0;
+}
