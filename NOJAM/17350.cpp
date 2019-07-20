ef63c83bfed532aafcf5b20316a3c41b49b56244
doublik <doublik99@gmail.com>
2019-07-21 03:08:26 +0900

    Solve BOJ: 17350(2루수 이름이 뭐야)

---
+++ NOJAM/17350.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int main() {
+  cin >> N;
+  while(N--) {
+    string s;
+    cin >> s;
+    if(s=="anj") {
+      cout << "뭐야;";
+      return 0;
+    }
+  }
+  cout << "뭐야?";
+  return 0;
+}
