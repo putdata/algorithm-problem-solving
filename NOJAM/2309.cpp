e9c2c8a2cca8a3e7184c5f1526f7d3c310a3ab1c
doublik <doublik99@gmail.com>
2019-07-21 13:37:12 +0900

    Solve BOJ: 2309(일곱 난쟁이)

---
+++ NOJAM/2309.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[9],sum;
+int main() {
+  for(int i=0; i<9; i++) {
+    cin>>a[i];
+    sum+=a[i];
+  }
+  sort(a,a+9);
+  for(int i=0; i<9; i++) {
+    for(int j=i+1; j<9; j++) {
+      if(sum-a[i]-a[j]==100) {
+        for(int k=0; k<9; k++) {
+          if(k!=i && k!=j) cout<<a[k]<<'\n';
+        }
+        return 0;
+      }
+    }
+  }
+  return 0;
+}
