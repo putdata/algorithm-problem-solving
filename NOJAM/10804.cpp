34046c76261489253ab52afbf91d0c3970bbd74f
doublik <doublik99@gmail.com>
2019-07-21 13:41:12 +0900

    Solve BOJ: 10804(카드 역배치)

---
+++ NOJAM/10804.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[22];
+int main() {
+  for(int i=1; i<=20; i++) arr[i]=i;
+  for(int i=0; i<10; i++) {
+    int a,b;
+    cin >> a >> b;
+    reverse(arr+a,arr+b+1);
+  }
+  for(int i=1; i<=20; i++) cout << arr[i] << ' ';
+  return 0;
+}
