b1f62477d8d131144069e2b0475d9285047a5481
doublik <doublik99@gmail.com>
2019-07-28 21:42:22 +0900

    Solve BOJ: 5691(평균 중앙값 문제)

---
+++ NOJAM/5691.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  while(1) {
+    int a,b;
+    cin >> a >> b;
+    if(a==0) break;
+    cout << a-(b-a) << '\n';
+  }
+  return 0;
+}
