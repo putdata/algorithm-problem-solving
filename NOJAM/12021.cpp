bcdd54a20cbc68c138bb5af5355ca74385749fd1
doublik <doublik99@gmail.com>
2019-08-04 14:27:19 +0900

    Solve BOJ: 12021(보물 찾기)

---
+++ NOJAM/12021.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double a,b;
+    cin >> a >> b;
+    cout << fixed;
+    cout.precision(3);
+    cout << sqrt(a*b) << ' ' << sqrt(a*b);
+    return 0;
+}
