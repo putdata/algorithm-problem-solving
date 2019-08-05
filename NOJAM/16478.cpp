60e7e4b65e3de32a0cbac74a7212238114008b93
doublik <doublik99@gmail.com>
2019-08-05 22:43:38 +0900

    Solve BOJ: 16478(원의 분할)

---
+++ NOJAM/16478.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long double pab,pbc,pcd;
+    cin >> pab >> pbc >> pcd;
+    printf("%Lf",(pab*pcd)/pbc);
+    // cout << (pab*pcd)/pbc;
+    return 0;
+}
