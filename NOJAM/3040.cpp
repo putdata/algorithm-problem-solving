7d4fcceabcda0a024ac42632f915c51c23b3df16
doublik <doublik99@gmail.com>
2019-08-10 13:30:54 +0900

    Solve BOJ: 3040(백설 공주와 일곱 난쟁이)

---
+++ NOJAM/3040.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a[9], sum=0;
+    for(int i = 0; i < 9; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    for(int i = 0; i < 9; i++) {
+        for(int j = i + 1; j < 9; j++) {
+            if(sum - (a[i] + a[j]) == 100) {
+                for(int k = 0; k < 9; k++) {
+                    if(k != i && k != j) cout << a[k] << "\n";
+                }
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
