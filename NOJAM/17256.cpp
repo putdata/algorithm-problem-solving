40d16f74e676881b81a5fa8da040bf6a16c2bc5c
doublik <doublik99@gmail.com>
2019-08-15 21:40:01 +0900

    Solve BOJ: 17256(달달함이 넘쳐흘러)

---
+++ NOJAM/17256.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int ax, ay, az, cx, cy, cz;
+  cin >> ax >> ay >> az >> cx >> cy >> cz;
+  cout << cx - az << ' ' << cy / ay << ' ' << cz - ax;
+  return 0;
+}
