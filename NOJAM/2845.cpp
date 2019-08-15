e0cd70b6e992d8e60d8fab0409028ba464190845
doublik <doublik99@gmail.com>
2019-08-15 18:17:49 +0900

    Solve BOJ: 2845(파티가 끝나고 난 뒤)

---
+++ NOJAM/2845.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int l, p;
+  cin >> l >> p;
+  for (int i = 0; i < 5; i++) {
+    int x;
+    cin >> x;
+    cout << x - (l * p) << ' ';
+  }
+  return 0;
+}
