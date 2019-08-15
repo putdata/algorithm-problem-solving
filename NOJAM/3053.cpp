804522294ee0fc9b8fde0d6f5d0d1fb47ad8540d
doublik <doublik99@gmail.com>
2019-08-15 15:46:39 +0900

    Solve BOJ: 3053(택시 기하학)

---
+++ NOJAM/3053.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  double r;
+  cin >> r;
+  cout.precision(6);
+  cout << fixed <<  M_PI * r * r << '\n' << r * (2 * r);
+  return 0;
+}
