4d271376e383e2dc6d958df31467ac0afc91cc37
doublik <doublik99@gmail.com>
2019-08-15 18:23:05 +0900

    Solve BOJ: 10179(쿠폰)

---
+++ NOJAM/10179.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  cout.precision(2);
+  cout << fixed;
+  while (t--) {
+    double x;
+    cin >> x;
+    cout << "$" << 0.8 * x << '\n';
+  }
+  return 0;
+}
