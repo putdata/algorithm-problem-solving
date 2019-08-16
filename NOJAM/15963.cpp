962d15a9faa7c44232d8aeda782aa7ec532cb58f
doublik <doublik99@gmail.com>
2019-08-16 12:14:14 +0900

    Solve BOJ: 15963(CASIO)

---
+++ NOJAM/15963.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long n, m;
+  cin >> n >> m;
+  if (n == m) cout << 1;
+  else cout << 0;
+  // cout << ((n & m) == n);
+  return 0;
+}
