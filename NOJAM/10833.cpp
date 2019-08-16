3f507a06128828a0e7589ce1e2fe13a55a69d37f
doublik <doublik99@gmail.com>
2019-08-16 11:19:09 +0900

    Solve BOJ: 10833(사과)

---
+++ NOJAM/10833.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, ans = 0;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    int a, b;
+    cin >> a >> b;
+    ans += b % a;
+  }
+  cout << ans;
+  return 0;
+}
