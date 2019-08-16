89df10a296e07508e3133101a665493a8f459b8b
doublik <doublik99@gmail.com>
2019-08-16 11:22:58 +0900

    Solve BOJ: 4101(크냐?)

---
+++ NOJAM/4101.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  while (1) {
+    int a, b;
+    cin >> a >> b;
+    if (!a) break;
+    a > b ? cout << "Yes\n" : cout << "No\n";
+  }
+  return 0;
+}
