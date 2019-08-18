c209ade4fc36adb7976079b9853cff1098a12345
doublik <doublik99@gmail.com>
2019-08-18 17:23:36 +0900

    Solve BOJ: 5086(배수와 약수)

---
+++ NOJAM/5086.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  while (1) {
+    unsigned a, b;
+    cin >> a >> b;
+    if (a == 0) break;
+    if (a % b == 0) cout << "multiple\n";
+    else if (b % a == 0) cout << "factor\n";
+    else cout << "neither\n";
+  }  
+  return 0;
+}
