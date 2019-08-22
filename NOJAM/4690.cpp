b0eb1f742ad0a2cbfec9c9c18d9b0d087ebde747
doublik <doublik99@gmail.com>
2019-08-22 20:52:06 +0900

    Solve BOJ: 4690(완전 세제곱)

---
+++ NOJAM/4690.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  for (int i = 6; i < 101; i++)
+    for (int j = 2 ; j < i; j++)
+      for (int k = j + 1; k < i; k++)
+        for (int l = k + 1; l < i; l++)
+          if (i * i * i == j * j * j + k * k * k + l * l * l)
+            cout << "Cube = " << i << ", Triple = (" << j << ',' << k << ',' << l << ")\n";
+  return 0;
+}
