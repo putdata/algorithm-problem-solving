e9c3aa12ade235c2b2e947deb2aebc282e5d0140
doublik <doublik99@gmail.com>
2019-08-23 18:03:01 +0900

    Solve BOJ: 10599(페르시아의 왕들)

---
+++ NOJAM/10599.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  while (1) {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a == 0 && b == 0 && c == 0 && d == 0) break;
+    cout << c - b << ' ' << d - a << '\n';
+  }
+  return 0;
+}
