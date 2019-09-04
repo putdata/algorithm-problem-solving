ca874fce62bad0a428c8d5ff6fdb99f9bedc1c4d
doublik <doublik99@gmail.com>
2019-09-04 13:23:28 +0900

    Solve BOJ: 15610(Abbey Courtyard)

---
+++ NOJAM/15610.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  long long a; cin >> a;
+  cout.precision(8);
+  cout << fixed << sqrt(a) * 4.0;
+  return 0;
+}
