62049d2ddd80b13f2fe3674b6300e355c218411a
doublik <doublik99@gmail.com>
2019-09-15 19:29:48 +0900

    Solve BOJ: 15272(Hissing Microphone)

---
+++ NOJAM/15272.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  string s; cin >> s;
+  s.find("ss") == string::npos ? cout << "no hiss" : cout << "hiss";
+  return 0;
+}
