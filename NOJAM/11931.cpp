ab7f593acd11f927e5d4881084f4575c6e59d45b
doublik <doublik99@gmail.com>
2019-09-23 10:35:01 +0900

    Solve BOJ: 11931(수 정렬하기 4)

---
+++ NOJAM/11931.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int n; cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++) cin >> a[i];
+  sort(a, a + n, greater<int>());
+  for (auto i : a) cout << i << '\n';
+  return 0;
+}
