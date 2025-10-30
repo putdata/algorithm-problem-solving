d60d0880d4a0ad89498275e116818d87d3a53742
ikjunoh <put_data@naver.com>
2025-10-30 22:07:27 +0900

    [Platinum IV] Title: 축구 전술, Time: 224 ms, Memory: 16920 KB -BaekjoonHub

---
+++ 백준/Platinum/3977. 축구 전술/축구 전술.cc
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL_TEST
+constexpr bool isLOCAL = true;
+#else
+constexpr bool isLOCAL = false;
+#endif
+
+#define debug if constexpr (isLOCAL) cerr
+
+typedef long long ll;
+typedef pair<int, int> pii;
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+vector<vector<int>> g;
+vector<vector<int>> comp;
+vector<int> id, low, sccId;
+stack<int> st;
+vector<bool> inSt;
+int timer = 0, sccCnt = 0;
+
+void tarjan(int cur) {
+  id[cur] = low[cur] = ++timer;
+  st.push(cur); inSt[cur] = 1;
+
+  for (int next : g[cur]) {
+    if (!id[next]) {
+      tarjan(next);
+      low[cur] = min(low[cur], low[next]);
+    } else if (inSt[next]) low[cur] = min(low[cur], id[next]);
+  }
+
+  if (id[cur] == low[cur]) {
+    vector<int> c;
+    while (1) {
+      int x = st.top(); st.pop(); inSt[x] = 0;
+      sccId[x] = sccCnt;
+      c.push_back(x);
+      if (x == cur) break;
+    }
+    sort(all(c));
+    comp.push_back(c);
+    sccCnt++;
+  }
+}
+
+void solve() {
+  int n, m; cin >> n >> m;
+  g.assign(n, {}); id.assign(n, 0); low.assign(n, 0); sccId.assign(n, 0); inSt.assign(n, 0);
+  comp = {};
+  timer = sccCnt = 0;
+
+  for (int i = 0; i < m; i++) {
+    int x, y; cin >> x >> y;
+    g[x].push_back(y);
+  }
+  for (int i = 0; i < n; i++) if (!id[i]) tarjan(i);
+
+  vector<vector<int>> dag(sccCnt);
+  vector<int> indeg(sccCnt);
+  for (int i = 0; i < n; i++) for (auto v : g[i]) {
+    int x = sccId[i], y = sccId[v];
+    if (x != y) {
+      dag[x].push_back(y);
+      indeg[y]++;
+    }
+  }
+  for (int i = 0; i < sccCnt; i++) {
+    auto &adj = dag[i];
+    sort(all(adj));
+    adj.erase(unique(all(adj)), adj.end());
+  }
+  int start = -1, cnt = 0;
+  for (int i = 0; i < sccCnt; i++) if (!indeg[i]) start = i, cnt++;
+  if (cnt != 1) cout << "Confused\n";
+  else {
+    vector<bool> vis(sccCnt);
+    queue<int> q; q.push(start); vis[start] = 1;
+    int val = 1;
+    while (!q.empty()) {
+      int cur = q.front(); q.pop();
+      for (auto next : dag[cur]) if (!vis[next]) {
+        q.push(next);
+        vis[next] = 1;
+        val++;
+      }
+    }
+    if (val != sccCnt) cout << "Confused\n";
+    else {
+      for (auto x : comp[start]) cout << x << '\n';
+    }
+  }
+  cout << "\n";
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int t; cin >> t;
+  while (t--) solve();
+  debug << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+  return 0;
+}
