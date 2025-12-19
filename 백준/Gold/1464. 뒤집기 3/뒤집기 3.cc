9a7f42c30a999ed83eb11e0d9f57026809de96a1
ikjunoh <put_data@naver.com>
2025-12-19 15:34:36 +0900

    [Gold IV] Title: 뒤집기 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1464. 뒤집기 3/뒤집기 3.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL_TEST
+constexpr bool isLOCAL = true;
+#else
+constexpr bool isLOCAL = false;
+#endif
+
+#define debug if constexpr (isLOCAL) cerr
+
+typedef long long ll;
+typedef pair<int, int> pii;
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+void solve() {
+  string s; cin >> s;
+  string ans = "";
+  for (auto x : s) {
+    if (x + ans <= ans + x) ans = x + ans;
+    else ans = ans + x;
+  }
+  cout << ans;
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  solve();
+  debug << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+  return 0;
+}
