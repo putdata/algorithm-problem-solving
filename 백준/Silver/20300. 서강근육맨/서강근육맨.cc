34304ffe164641ef6662fbc311372a05a8028d07
ikjunoh <put_data@naver.com>
2025-12-23 01:34:44 +0900

    [Silver III] Title: 서강근육맨, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL_TEST
+constexpr bool isLOCAL = true;
+#else
+constexpr bool isLOCAL = false;
+#endif
+
+#define debug if constexpr (isLOCAL) cerr
+
+typedef long long ll;
+typedef pair<int, int> pii;
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+void solve() {
+  int n; cin >> n;
+  vector<ll> vec(n);
+  for (auto &x : vec) cin >> x;
+  sort(all(vec));
+  ll ans = vec[n - 1];
+  for (int i = 0; i < n / 2; i++) {
+    ans = max(ans, vec[i] + vec[n - 1 - i - (n % 2 == 1)]);
+  }
+  cout << ans;
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  solve();
+  debug << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+  return 0;
+}
