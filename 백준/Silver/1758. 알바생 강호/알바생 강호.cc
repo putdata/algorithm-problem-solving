ecaa72a5ce3846b36fd1dde95b42cde8bd933885
ikjunoh <put_data@naver.com>
2025-12-23 01:34:53 +0900

    [Silver IV] Title: 알바생 강호, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL_TEST
+constexpr bool isLOCAL = true;
+#else
+constexpr bool isLOCAL = false;
+#endif
+
+#define debug if constexpr (isLOCAL) cerr
+
+typedef long long ll;
+typedef pair<int, int> pii;
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+void solve() {
+  int n; cin >> n;
+  vector<int> vec(n);
+  for (auto &x : vec) cin >> x;
+  sort(vec.rbegin(), vec.rend());
+  ll ans = 0;
+  for (int i = 0; i < n; i++) {
+    ans += max(vec[i] - i, 0);
+  }
+  cout << ans;
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  solve();
+  debug << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+  return 0;
+}
