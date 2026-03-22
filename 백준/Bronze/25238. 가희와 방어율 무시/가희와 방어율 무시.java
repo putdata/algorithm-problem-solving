e4881fbcc204489777750a93155e050d50262036
오익준(Ikjun Oh) <ikjun.oh@musinsa.com>
2026-03-22 16:10:52 +0900

    [Bronze 4] Title: 가희와 방어율 무시, Time: 104 ms, Memory: 14420 KB - BOJ Helper

---
+++ 백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static final PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer st;
+
+    public static int nextInt() throws IOException {
+        if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(in.readLine());
+        return Integer.parseInt(st.nextToken());
+    }
+
+    public static void main(String[] args) throws Exception {
+        int a = nextInt(), b = nextInt();
+        if (a * (100 - b) >= 10000) out.println("0");
+        else out.println("1");
+        out.close();
+    }
+}
