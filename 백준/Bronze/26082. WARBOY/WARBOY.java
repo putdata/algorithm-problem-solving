ad977c3821e52dacd9089c127c8635d4681a3b37
오익준(Ikjun Oh) <ikjun.oh@musinsa.com>
2026-03-24 23:33:50 +0900

    [Bronze 5] Title: WARBOY, Time: 108 ms, Memory: 14364 KB - BOJ Helper

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static final PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer st;
+
+    public static int nextInt() throws IOException {
+        if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(in.readLine());
+        return Integer.parseInt(st.nextToken());
+    }
+
+    public static void main(String[] args) throws Exception {
+        int a = nextInt(), b = nextInt(), c = nextInt();
+        out.println(b / a * c * 3);
+        out.close();
+    }
+}
